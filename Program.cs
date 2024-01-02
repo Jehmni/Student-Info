8945c3dbfa62d2daec371d7097a94e953cda65a2
Uncle Pi <144686415+Jehmni@users.noreply.github.com>
2024-01-02 08:45:52 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assessment1
+{
+    internal class Programd
+    { 
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter the number of students
+            Console.WriteLine("Enter the number of students: ");
+            int arraySize = int.Parse(Console.ReadLine());
+
+            // Create an array to store information for students
+            Student[] AiStudents = new Student[arraySize];
+
+            // Loop through the array and prompt the user to enter information for each student
+            for (int i = 0; i < AiStudents.Length; i++)
+            {
+                Console.WriteLine("Enter information for student {0}", i + 1);
+
+                // Prompt the user to enter the student's name
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                // Prompt the user to enter the student's age
+                Console.Write("Age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                // Prompt the user to enter the student's ID
+                Console.Write("Student ID: ");
+                string id = Console.ReadLine();
+
+                // Create a new Student object with the input values and add it to the array
+                AiStudents[i] = new Student { Name = name, Age = age, Id = id };
+            }
+
+            // Print out the information of each student in the array
+            for (int i = 0; i < AiStudents.Length; i++)
+            {
+                AiStudents[i].GetInformation();
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Student
+{
+	private string name, id;
+	private int age;
+	
+	// Constructors (default constructor and for when values are provided)
+	public Student()
+	{
+		this.name= null;
+		this.age= 0;
+		this.id= "-";
+	}
+	public Student(string studName, int age, string studId)
+	{
+		this.name = studName;
+		this.age = age;	
+		this.id = studId;
+	}
+
+
+	// Properties getters and setters
+	public string Name {get {return name; } set { name = value; } }
+	public int Age { get {return age; } set { age = value; } }
+	public string Id { get {return id; } set { id = value; } }
+
+	// Method to write student information on screen
+	public void GetInformation()
+	{
+		Console.WriteLine(name + ", with Student-ID " +id+ " is " +age+ " years old.");
+		Console.WriteLine();
+	}
+	
+}
+
